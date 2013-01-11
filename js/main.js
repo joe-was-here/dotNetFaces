11520acaa26487df29493e66996c99aabd7cf81f
Joe Madden <joe@beardbro.com>
2013-01-11 16:02:49 -0500

    js for profile page and adding rage faces

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
     $('.team img').rwdImageMaps();
 
     $('area').hover(function(){
-        var that = $(this),
-             areas = $('area');
+        var that = $(this);
+        setTimeout(function() {
+            $('.' + that.attr('id')).stop().show(0).removeClass('hidden');
+        }, 50);
     },
-    function(){
-
+    function () {
+        var that = $(this);
+        setTimeout(function() {
+            $('.' + that.attr('id')).hide(0).addClass('hidden');
+        }, 50);
     });
 
 });
