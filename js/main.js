9dee639b7b79c805bec71d01f39bf82b1c4c9df5
Joe Madden <joe@beardbro.com>
2013-01-11 13:21:19 -0500

    setting up the home page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
+    $('.team img').rwdImageMaps();
 
+    $('area').hover(function(){
+        var that = $(this),
+             areas = $('area');
+    },
+    function(){
+
+    });
+
+});
